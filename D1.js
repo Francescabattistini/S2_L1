216c87445e1e8966ae2796b053e848688d9da66d
Francescabattistini <166622198+Francescabattistini@users.noreply.github.com>
2024-06-17 18:00:13 +0200

    Update D1.js

--- D1.js
+++ D1.js
@@ -61,4 +61,4 @@ let name1 = "john";
 let name2 = "John";
 console.log("name1 e name2 sono uguali?", name1 === name2);
 console.log(name1 === name2.toLowerCase());
-console.log(name1.toUpperCase(), name2.toUpperCase());
+console.log(name1.toLowerCase(), name2.toLowerCase());
